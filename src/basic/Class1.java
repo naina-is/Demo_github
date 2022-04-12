75dafacb3b3f74664f902b0b231af2f610d09caa
naina-is <islam.naina15@gmail.com>
2022-04-12 12:27:13 -0400

    first commit

---
+++ src/basic/Class1.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello everyone");
+	}
+	
+}
---
+++ src/basic/Class2.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+}
